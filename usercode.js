fba985b31a4f936d29a330de6c0809bca64de00c
Maksim Kotlyar <kotlyar.maksim@gmail.com>
2020-03-16 18:46:20 +0200

    init

---
+++ usercode.js
@@ -0,0 +1,10 @@
+const moment = require('moment');
+const axios = require('axios');
+
+module.exports = (data) => {
+    console.log(moment().format('MMMM Do YYYY, h:mm:ss a'));
+
+    data.foo = 1;
+
+    return data;
+};
